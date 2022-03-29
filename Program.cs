1d8858d27956a6e90cba2e0167014e47c941e749
STUDENT <STUDENT@DESKTOP-1IC7LVA>
2022-03-29 16:07:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using Masini;
+using Fisier;
+namespace TargDeMasini
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            AdministrareFisier adminMasini = new AdministrareFisier(numeFisier);
+            TargMasini MasinaNoua = new TargMasini();
+            int nrMasini = 0;
+            adminMasini.GetMasini(out nrMasini);
+
+            string optiune;
+            do
+            {
+                Console.WriteLine("C. Citire informatii despre masina de la tastatura");
+                Console.WriteLine("A. Afisarea ultima masina introdusa");
+                Console.WriteLine("L. Cautare dupa numele vanzatorului si a cumparatorului");
+                Console.WriteLine("F. Afisare masini din fisier");
+                Console.WriteLine("S. Salvare masina in fisier");
+                Console.WriteLine("D. Citirea argumentelor din linia de comanda");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "C":
+                        MasinaNoua = CitireMasinaTastatura();
+
+                        break;
+                    case "A":
+                        AfisareMasina(MasinaNoua);
+                        break;
+                    case "F":
+                        TargMasini[] masini = adminMasini.GetMasini(out nrMasini);
+                        AfisareMasini(masini, nrMasini);
+                        break;
+
+//Lab 3 cautarea dupa nume(pana acum)
+                    case "L":
+                        Console.WriteLine("Introduceti numele vanzatorului");
+                        string var_vanzator=Console.ReadLine();
+                        Console.WriteLine("Introduceti numele cumparatorului");
+                        string var_cumparator=Console.ReadLine();
+                        Console.WriteLine(adminMasini.GetMasina(var_vanzator,var_cumparator));
+                        break;
+//Lab 3 salvarea datelor
+                    case "S":
+                        int idMasina = nrMasini + 1;
+                        MasinaNoua.SetIdMasina(idMasina);
+                        adminMasini.AddMasina(MasinaNoua);
+                        nrMasini = nrMasini + 1;
+
+                        break;
+
+//Lab 4 citirea argumentelor
+                    case "D":
+                        if (args.Length == 0)
+                            Console.Write("Linia de comanda nu contine argumente\n\n");
+                        else
+                        {
+                            Console.WriteLine("Numarul de argumente este: {0}", args.Length);
+                            foreach (string param in args)
+                                Console.WriteLine(param);
+                            switch (args[0])
+                            {
+                                case "about":
+                                    Console.WriteLine("Targ de masini");
+                                       break;
+                                case "despre":
+                                    Console.WriteLine("Masini de vanzare");
+                                    break;
+                                default:
+                                    Console.WriteLine("Comanda necunoscuta");
+                                    break;
+                            }
+                        }
+
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+            Console.ReadKey();
+        }
+
+        public static void AfisareMasina(TargMasini student)
+        {
+            string infoStudent = string.Format("Masina cu id-ul #{0} are specificatiile: Nume Vanzator: {1}. Nume Cumparator: {2}. Firma: {3}. Model: {4}. An Fabricatie: {5}." +
+                "Culoarea: {6}. Optiuni: {7}. Data tranzactiei: {8}. Pret: {9}.",
+                   student.GetIdMasina(),
+                   student.GetNumeVanzator() ?? " NECUNOSCUT ",
+                   student.GetNumeCumparator() ?? " NECUNOSCUT ",
+                   student.GetFirma() ?? "NECUNOSCUT ",
+                   student.GetModel() ?? " NECUNOSCUT ",
+                   student.GetAnFab() ?? "NECUNOSCUT ",
+                   student.GetCuloare() ?? "NECUNOSCUT ",
+                   student.GetOptiuni() ?? "NECUNOSCUT ",
+                   student.GetDataTranz() ?? "NECUNOSCUT ",
+                   student.GetPret() ?? " NECUNOSCUT ");
+
+            Console.WriteLine(infoStudent);
+        }
+
+//Lab3 afisarea datelor din fisier
+        public static void AfisareMasini(TargMasini[] masini, int nrMasini)
+        {
+            Console.WriteLine("Informatii:");
+            for (int contor = 0; contor < nrMasini; contor++)
+            {
+                AfisareMasina(masini[contor]);
+            }
+        }
+
+
+//Lab 3 citire de la tastatura
+        public static TargMasini CitireMasinaTastatura()
+        {
+            Console.WriteLine("Introduceti numele vanzatorului ");
+            string numevanzator = Console.ReadLine();
+
+            Console.WriteLine("Introduceti nume cumparatorului ");
+            string numecumparator = Console.ReadLine();
+
+            Console.WriteLine("Introduceti firma masinei ");
+            string firma = Console.ReadLine();
+
+            Console.WriteLine("Introduceti modelul masinei");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Introduceti anul de fabricatie");
+            string an_fab = Console.ReadLine();
+
+            Console.WriteLine("Introduceti culoarea");
+            string culoare = Console.ReadLine();
+
+            Console.WriteLine("Introduceti optiuni");
+            string optiuni = Console.ReadLine();
+
+            Console.WriteLine("Introduceti data tranzactie");
+            string data_tranz = Console.ReadLine();
+
+            Console.WriteLine("Introduceti pretul");
+            string pret = Console.ReadLine();
+
+            TargMasini student = new TargMasini(0, numevanzator, numecumparator, model, firma, an_fab, culoare, optiuni, data_tranz, pret);
+
+            return student;
+        }
+    }
+}
